dbe01b4fa5cdb855ea4c84c8ab7e153a6274decd
Nico7522 <nico.daddabbo7100#gmail.com>
2023-04-27 09:37:16 +0200

    navbar + font

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
+import NavBar from './components/navbar'
 
 function App() {
  
 
   return (
     <>
-      <h1 className='text-center font'>RECIPE HOME</h1>
+    <NavBar />
+      <h1 className='text-center font mt-24'>RECIPE HOME</h1>
 
-      <div className='mx-auto text-center w-10/12 h-96 border-4 border-indigo-500/100'>
+      <div className='m-auto text-center w-10/12 h-96 border-4 border-indigo-500/100'>
         ALL RECIPE
         <div className='mx-auto mt-5 w-10/12 border-4 border-indigo-500/100'>RECIPE</div>
         <div className='mx-auto mt-5 w-10/12 border-4 border-indigo-500/100'>RECIPE</div>
